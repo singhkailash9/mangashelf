bb6245f30453fec45520cd8b26f5b58405f8ef2c
Kailash Singh <singhkailash.sk9@gmail.com>
2026-04-08 21:47:17 +0530

    Fix cors bug

--- server/server.js
+++ server/server.js
@@ -8,13 +8,13 @@ const authRoutes = require("./routes/authRoutes");
 
 const app = express();
 
-app.use(express.json());
-app.use(cookieParser);
-app.use("/api/auth", authRoutes);
 app.use(cors({
     origin: "http://localhost:5173",
     credentials:  true
 }));
+app.use(express.json());
+app.use(cookieParser());
+app.use("/api/auth", authRoutes);
 
 mongoose.connect(process.env.MONGO_URI).then(console.log("MongoDB connected")).catch((err)=>console.error(`MongoDB error: ${err}`));
 
